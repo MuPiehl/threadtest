0a0ff79f3cced453c40aeed1d0d486ff5083f9ae
MuPiehl <matthias.piehl@hypoport.de>
2015-09-04 01:28:36 +0200

    test1

--- src/main/java/testThread/App.java
+++ src/main/java/testThread/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
         List<String> myList = Arrays.asList("element1", "element2",
-                "element3", "elem 4/3",
+                "element3", "element 4/3",
                 "element5", "elem 7/6",
                 "ELEMENT 8", "999");
 
