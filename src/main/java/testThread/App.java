eedddfd5059c0922fdf79716fb2460e9a25506b5
MuPiehl <matthias.piehl@hypoport.de>
2015-09-04 00:47:20 +0200

    test1

--- src/main/java/testThread/App.java
+++ src/main/java/testThread/App.java
@@ -1,5 +1,8 @@
 package testThread;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,12 @@ public class App
 {
     public static void main( String[] args )
     {
+        List<String> myList = Arrays.asList("element1", "element2",
+                "element3", "elem 4/3",
+                "ELEMENT 8", "999");
+
+        myList.forEach(element -> System.out.println(element));
+
         System.out.println( "Hello World!" );
     }
 }
--- src/test/java/testThread/AppTest.java
+++ src/test/java/testThread/AppTest.java
@@ -3,37 +3,15 @@ package testThread;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.testng.Assert;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
+    @org.testng.annotations.Test
+    public void testApp() {
         System.err.println("test by MPi");
-        assertTrue( true );
+//        Assert.assertTrue(false);
+        Assert.assertTrue(true);
     }
 }
